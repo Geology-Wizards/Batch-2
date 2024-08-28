c0f58a4336d53a30b2e2a5230546f4cc221e400f
Geology-Wizards <wgeology@gmail.com>
2024-08-28 23:33:20 +0530

    Create take-test.php

---
+++ geology-wizards/tests-quizzes/take-test.php
@@ -0,0 +1,12 @@
+<?php
+  // Header
+  include 'header.php';
+
+  // Content
+  ?>
+  <h1>Take Test</h1>
+  <p>Take a test for the <?php echo $_GET['batch']; ?> batch:</p>
+  <!-- Test questions and answers code -->
+
+  // Footer
+  <?php include 'footer.php'; ?>
