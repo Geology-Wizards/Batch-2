4d2e923ccbca65263c04fbe115c83d9ac6d239a0
Geology-Wizards <wgeology@gmail.com>
2024-08-28 23:28:30 +0530

    Create contact.php

---
+++ geology-wizards/contact.php
@@ -0,0 +1,14 @@
+<?php
+  // Header
+  include 'header.php';
+
+  // Content
+  ?>
+  <h1>Contact Us</h1>
+  <p>Get in touch with us for any questions or feedback.</p>
+  <form>
+    <!-- Contact form fields -->
+  </form>
+
+  // Footer
+  <?php include 'footer.php'; ?>
