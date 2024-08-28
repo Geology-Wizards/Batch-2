fdc2dbf5ae21b0d7776827ea4e2ed19b9cccd952
Geology-Wizards <wgeology@gmail.com>
2024-08-28 23:34:00 +0530

    Create upload-pyq.php

---
+++ geology-wizards/previous-years-questions/upload-pyq.php
@@ -0,0 +1,13 @@
+<?php
+  // Header
+  include 'header.php';
+
+  // Content
+  ?>
+  <h1>Upload Previous Years' Questions</h1>
+  <p>Upload previous years' questions for the <?php echo $_GET['batch']; ?> batch:</p>
+  <form>
+    <!-- PYQ upload form fields -->
+  </form>
+
+ 
