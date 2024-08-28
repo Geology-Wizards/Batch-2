7bf38bdc20820c915908fa1659b40488a1660ce7
Geology-Wizards <wgeology@gmail.com>
2024-08-28 23:36:51 +0530

    Create profile.php

---
+++ geology-wizards/user-accounts/profile.php
@@ -0,0 +1,12 @@
+<?php
+  // Header
+  include 'header.php';
+
+  // Content
+  ?>
+  <h1>Profile</h1>
+  <p>View and edit your profile:</p>
+  <!-- Profile information and edit form -->
+
+  // Footer
+  <?php include 'footer.php'; ?>
