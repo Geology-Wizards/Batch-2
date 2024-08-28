97548555df44fc4f497b5a14b95dd8af92fc508d
Geology-Wizards <wgeology@gmail.com>
2024-08-28 23:31:38 +0530

    Create video-player.php

---
+++ geology-wizards/recorded-videos/video-player.php
@@ -0,0 +1,12 @@
+<?php
+  // Header
+  include 'header.php';
+
+  // Content
+  ?>
+  <h1>Video Player</h1>
+  <p>Watch videos for the <?php echo $_GET['batch']; ?> batch:</p>
+  <!-- Video player code -->
+
+  // Footer
+  <?php include 'footer.php'; ?>
