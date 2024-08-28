ab8e8e73d85bbd982d02742a056d01039d2e3d7d
Geology-Wizards <wgeology@gmail.com>
2024-08-28 23:37:33 +0530

    Create forum.php

---
+++ geology-wizards/discussion-forums/forum.php
@@ -0,0 +1,12 @@
+<?php
+  // Header
+  include 'header.php';
+
+  // Content
+  ?>
+  <h1>Forum</h1>
+  <p>Discuss geology-related topics:</p>
+  <!-- Forum discussion code -->
+
+  // Footer
+  <?php include 'footer.php'; ?>
