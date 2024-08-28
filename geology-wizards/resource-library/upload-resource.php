8a57214c8191c9ad0f58d52bfa70546149566f7c
Geology-Wizards <wgeology@gmail.com>
2024-08-28 23:38:09 +0530

    Create upload-resource.php

---
+++ geology-wizards/resource-library/upload-resource.php
@@ -0,0 +1,14 @@
+<?php
+  // Header
+  include 'header.php';
+
+  // Content
+  ?>
+  <h1>Upload Resource</h1>
+  <p>Upload a new resource:</p>
+  <form>
+    <!-- Resource upload form fields -->
+  </form>
+
+  // Footer
+  <?php include 'footer.php'; ?>
