508d9d453d36d3446b4aeb971b0e509cd003e8b3
Geology-Wizards <wgeology@gmail.com>
2024-08-28 23:36:19 +0530

    Create login.php

---
+++ geology-wizards/user-accounts/login.php
@@ -0,0 +1,14 @@
+<?php
+  // Header
+  include 'header.php';
+
+  // Content
+  ?>
+  <h1>Login</h1>
+  <p>Login to your account:</p>
+  <form>
+    <!-- Login form fields -->
+  </form>
+
+  // Footer
+  <?php include 'footer.php'; ?>
