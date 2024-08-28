a27ec206fd058d5a97ffd4ba5ca3264feae8d358
Geology-Wizards <wgeology@gmail.com>
2024-08-28 23:30:53 +0530

    Create upload-video.php

---
+++ geology-wizards/recorded-videos/upload-video.php
@@ -0,0 +1,14 @@
+<?php
+  // Header
+  include 'header.php';
+
+  // Content
+  ?>
+  <h1>Upload Video</h1>
+  <p>Upload a new video for the <?php echo $_GET['batch']; ?> batch:</p>
+  <form>
+    <!-- Video upload form fields -->
+  </form>
+
+  // Footer
+  <?php include 'footer.php'; ?>
