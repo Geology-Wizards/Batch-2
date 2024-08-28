b336d971b85c346db5dae7fc7cb5661dbc719879
Geology-Wizards <wgeology@gmail.com>
2024-08-28 23:39:54 +0530

    Create faq.php

---
+++ geology-wizards/faq/faq.php
@@ -0,0 +1,12 @@
+<?php
+  // Header
+  include 'header.php';
+
+  // Content
+  ?>
+  <h1>Frequently Asked Questions</h1>
+  <p>Find answers to common questions:</p>
+  <!-- FAQ list -->
+
+  // Footer
+  <?php include 'footer.php'; ?>
