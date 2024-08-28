2d88fec5c3c7b3b51f84c0b2ad04e9d781982e4c
Geology-Wizards <wgeology@gmail.com>
2024-08-28 23:32:32 +0530

    Create create-test.php

---
+++ geology-wizards/tests-quizzes/create-test.php
@@ -0,0 +1,14 @@
+<?php
+  // Header
+  include 'header.php';
+
+  // Content
+  ?>
+  <h1>Create Test</h1>
+  <p>Create a new test for the <?php echo $_GET['batch']; ?> batch:</p>
+  <form>
+    <!-- Test creation form fields -->
+  </form>
+
+  // Footer
+  <?php include 'footer.php'; ?>
