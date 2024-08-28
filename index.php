a1efc430138059134cae0a972ebd4f2db7d43d7e
Geology-Wizards <wgeology@gmail.com>
2024-08-28 23:55:25 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+  // Header
+  include 'header.php';
+
+  // Content
+  ?>
+  <h1>Welcome to Geology Wizards</h1>
+  <p>Explore our resources and learn about geology!</p>
+
+  // Footer
+  <?php include 'footer.php'; ?>
