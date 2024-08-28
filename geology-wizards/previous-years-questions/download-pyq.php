b6d8f7330ded823e53dfe022b8a501f071503a23
Geology-Wizards <wgeology@gmail.com>
2024-08-28 23:35:01 +0530

    Create download-pyq.php

---
+++ geology-wizards/previous-years-questions/download-pyq.php
@@ -0,0 +1,12 @@
+<?php
+  // Header
+  include 'header.php';
+
+  // Content
+  ?>
+  <h1>Download Previous Years' Questions</h1>
+  <p>Download previous years' questions for the <?php echo $_GET['batch']; ?> batch:</p>
+  <!-- PYQ download code -->
+
+  // Footer
+  <?php include 'footer.php'; ?>
