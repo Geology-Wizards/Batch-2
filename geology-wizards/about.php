0f3cb22d9f4afd591c5959a7fa73dc089386465d
Geology-Wizards <wgeology@gmail.com>
2024-08-28 23:27:43 +0530

    Create about.php

---
+++ geology-wizards/about.php
@@ -0,0 +1,11 @@
+<?php
+  // Header
+  include 'header.php';
+
+  // Content
+  ?>
+  <h1>About Us</h1>
+  <p>Learn more about Geology Wizards and our mission.</p>
+
+  // Footer
+  <?php include 'footer.php'; ?>
