d87ee5bebf5504d4af5a40dc883a7bd84137f80a
Geology-Wizards <wgeology@gmail.com>
2024-08-28 23:35:46 +0530

    Create register.php

---
+++ geology-wizards/user-accounts/register.php
@@ -0,0 +1,14 @@
+<?php
+  // Header
+  include 'header.php';
+
+  // Content
+  ?>
+  <h1>Register</h1>
+  <p>Create a new account:</p>
+  <form>
+    <!-- Registration form fields -->
+  </form>
+
+  // Footer
+  <?php include 'footer.php'; ?>
