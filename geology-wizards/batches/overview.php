820b6bef2e66b500cc187a3af27a5a64d8c14597
Geology-Wizards <wgeology@gmail.com>
2024-08-28 23:29:19 +0530

    Create overview.php

---
+++ geology-wizards/batches/overview.php
@@ -0,0 +1,14 @@
+<?php
+  // Header
+  include 'header.php';
+
+  // Content
+  ?>
+  <h1>Batches Overview</h1>
+  <p>View all batches and their details.</p>
+  <ul>
+    <!-- List of batches with links to batch-details.php -->
+  </ul>
+
+  // Footer
+  <?php include 'footer.php'; ?>
